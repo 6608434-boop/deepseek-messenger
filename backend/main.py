4968619aa0bb39fe99ace4bb7aeee8b3f60fd247
6608434-boop <6608434-boop@users.noreply.github.com>
2026-02-19 20:56:14 +0500

    Add frontend domain to CORS

--- backend/main.py
+++ backend/main.py
@@ -66,6 +66,7 @@ app.add_middleware(
         "http://localhost:5500",
         "http://127.0.0.1:5500",
         "http://localhost:8000",
+        "https://rare-renewal-production.up.railway.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
